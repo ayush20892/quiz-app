167d0262e32beba52c4f4af94d018bd0db09bcc6
ayush20892 <ayush20892@gmail.com>
2021-06-08 00:16:36 +0530

    feat: quiz page

---
+++ src/page/quizPage.tsx
@@ -0,0 +1,14 @@
+import { Header } from "../component/header/header";
+import { QuizCard } from "../component/quizCard/quizCard";
+import { useParams } from "react-router";
+
+export function QuizPage() {
+  const { quizName, questionNumber } = useParams()
+  console.log(typeof +questionNumber)
+  return(
+    <div>
+      <Header heading={quizName} greeting={"Player : "} username={"Ayush"} />
+      <QuizCard quizName={quizName} questionNumber={+questionNumber}/>
+    </div>
+  );
+}
