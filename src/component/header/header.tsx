c8866ae2a82cd722b7ebdd50ba7ccbeaf30dbe94
ayush20892 <ayush20892@gmail.com>
2021-06-08 00:11:56 +0530

    feat: header component added

---
+++ src/component/header/header.tsx
@@ -0,0 +1,22 @@
+import "./header.css"
+import { useUser } from "../../context/userContext/userContext"
+
+type headerProps = {
+  heading: String,
+  greeting: String
+  username: String,
+}
+
+export function Header({ heading, greeting, username } : headerProps ) {
+  const { userState } = useUser()
+  console.log(userState)
+  return(
+    <header>
+      <h1>{heading}</h1>
+      <div className="header-details">
+        <h3>{greeting} {userState.userName}</h3>
+        <h4>Score: {userState.score}</h4>
+      </div>
+    </header>
+  )
+}
