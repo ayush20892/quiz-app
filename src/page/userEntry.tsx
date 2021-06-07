6d73345f43affb14109a5c8a0d9060ce74357041
ayush20892 <ayush20892@gmail.com>
2021-06-08 00:49:47 +0530

    refactor: unused imports removed

--- src/page/userEntry.tsx
+++ src/page/userEntry.tsx
@@ -1,13 +1,11 @@
 import { useState } from "react"
 import { useUser } from "../context/userContext/userContext"
-import { useNavigate, Link, Navigate } from "react-router-dom"
 import { refreshPage } from "../util"
 
 
 export function UserEntry () {
   const { userDispatch } = useUser()
   const [inputValue, setInputValue] = useState("")
-  const navigate = useNavigate()
   return(
     <div>
       <h1>Welcome to general fun quiz.</h1>
