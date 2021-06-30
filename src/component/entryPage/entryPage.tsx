448c8e04e9a02cbe2b3b3c6362c478dc594ad956
ayush20892 <ayush20892@gmail.com>
2021-07-01 00:12:18 +0530

    feat: network call added

--- src/component/entryPage/entryPage.tsx
+++ src/component/entryPage/entryPage.tsx
@@ -1,22 +1,30 @@
 import "./entryPage.css"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useUser } from "../../context/userContext/userContext";
+import { postNewUser } from "../../networkCalls";
 import { refreshPage } from "../../util"
+import axios from "axios";
 
 export function EntryPage() {
   const { userDispatch } = useUser()
-  const [inputValue, setInputValue] = useState("")
+  const [inputValue, setInputValue] = useState<string>("")
+
+  async function userNameSubmit() {
+    const res = await postNewUser(inputValue, userDispatch)
+    if(res === 1)
+      refreshPage()
+    console.log(res)
+  }
+
   return(
     <div className="entry-box">
       <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Enter username">
       </input>
       <div className="username-submit">
-        <button onClick={() => {
-          userDispatch({type: "ADD-USER", payload: inputValue});
-          refreshPage() }}>
+        <button onClick={userNameSubmit}>
             Submit
         </button>
       </div>
     </div>
-  );
+  )
 }
