9baec601db7bacde625bd81da8e4b0353c9f23fa
ayush20892 <ayush20892@gmail.com>
2021-06-30 23:19:28 +0530

    feat: header updated

--- src/component/header/header.tsx
+++ src/component/header/header.tsx
@@ -4,17 +4,20 @@ import { useUser } from "../../context/userContext/userContext"
 type headerProps = {
   heading: String,
   greeting: String
-  username: String,
 }
 
-export function Header({ heading, greeting, username } : headerProps ) {
+export function Header({ heading, greeting } : headerProps ) {
   const { userState } = useUser()
   return(
     <header>
       <h1>{heading}</h1>
       <div className="header-details">
-        <h3>{greeting} {userState.userName}</h3>
-        <h4>Score: {userState.score}</h4>
+        <h3>{greeting}&nbsp; 
+          <span>{userState.userName}</span>
+        </h3>
+        <h4>Score: &nbsp;
+          <span>{userState.score}</span>
+        </h4>
       </div>
     </header>
   )
