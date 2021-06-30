6011759e33cf853a2e26ab24defb0a5c5522b217
ayush20892 <ayush20892@gmail.com>
2021-07-01 00:16:49 +0530

    feat: network call added

--- src/component/quizResult/quizResult.tsx
+++ src/component/quizResult/quizResult.tsx
@@ -4,28 +4,37 @@ import { useQuiz } from "../../context/quizContext/quizContext"
 import { useEffect } from "react"
 import { Link } from "react-router-dom"
 import { useUser } from "../../context/userContext/userContext"
+import { updateUserScore } from "../../networkCalls"
 
 export function QuizResult () {
   const { quizState, quizDispatch } = useQuiz()
-  const { userDispatch } = useUser()
+  const { userState, userDispatch } = useUser()
 
   useEffect(() => {
     quizDispatch({type: "RIGHT-WRONG"})
-    userDispatch({type: "UPDATE-SCORE", payload: quizState.currentQuizScore})
     quizDispatch({type: "CLEAR-OPTIONS"})
   },[quizDispatch])
+
+  useEffect(() => {
+    (async () => {
+      await updateUserScore(userState, quizState.currentQuizScore, userDispatch)
+    })()
+  },[quizDispatch])
+
   return(
     <div className="quizResult">
       <div className="score">
         Quiz Score: <span>{quizState.currentQuizScore}</span>
       </div>
-      <div className="correctly-answered">
-        <AiOutlineCheck style={{fill: "green"}}/> <span>{quizState.userAnswer.right}</span>
-        Correct
-      </div>
-      <div className="wrongly-answered">
-        <AiOutlineClose style={{fill: "red"}}/> 
-        <span>{quizState.userAnswer.wrong}</span> Wrong
+      <div className="right-wrong">
+        <div className="correctly-answered answered-box">
+          <AiOutlineCheck style={{fill: "green"}}/> <span>{quizState.userAnswer.right}</span>
+          Correct
+        </div>
+        <div className="wrongly-answered answered-box">
+          <AiOutlineClose style={{fill: "red"}}/> 
+          <span>{quizState.userAnswer.wrong}</span> Wrong
+        </div>
       </div>
       <Link to="/">Home</Link>
     </div>
