a2bd6f7ecdf60df4aab393801dd9240463bdb5ab
Ayush Sharma <ayush20892@gmail.com>
2021-12-02 13:10:34 +0530

    post user changes

--- src/component/entryPage/entryPage.tsx
+++ src/component/entryPage/entryPage.tsx
@@ -1,29 +1,28 @@
-import "./entryPage.css"
-import { useState, useEffect } from "react";
+import "./entryPage.css";
+import { useState } from "react";
 import { useUser } from "../../context/userContext/userContext";
 import { postNewUser } from "../../networkCalls";
-import { refreshPage } from "../../util"
-import axios from "axios";
+import { refreshPage } from "../../util";
 
 export function EntryPage() {
-  const { userDispatch } = useUser()
-  const [inputValue, setInputValue] = useState<string>("")
+  const { userDispatch } = useUser();
+  const [inputValue, setInputValue] = useState<string>("");
 
   async function userNameSubmit() {
-    const res = await postNewUser(inputValue, userDispatch)
-    if(res === 1)
-      refreshPage()
+    const res = await postNewUser(inputValue, userDispatch);
+    if (res === 1) refreshPage();
   }
 
-  return(
+  return (
     <div className="entry-box">
-      <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Enter username">
-      </input>
+      <input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Enter username"
+      ></input>
       <div className="username-submit">
-        <button onClick={userNameSubmit}>
-            Submit
-        </button>
+        <button onClick={userNameSubmit}>Submit</button>
       </div>
     </div>
-  )
+  );
 }
